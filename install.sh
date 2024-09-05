c5fe3d4d3728b0c44a72b9f6ab5a79a7cc1b28fa
Phuc <phuc@phucngo.dev>
2024-09-05 09:31:24 +0700

    setup git diff git-delta

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "===============Install homebrew and software================="
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install tmux neovim fd ripgrep btop starship fzf glow go nodejs danvergara/tools/dblab postgresql@16 protobuf colima docker docker-compose kubectl mysql ansible rig
+brew install tmux neovim fd ripgrep btop starship fzf glow git-delta go nodejs postgresql@16 protobuf colima docker docker-compose kubectl mysql ansible rig
 
 echo "=====================Copy config files======================="
 
@@ -23,8 +23,6 @@ go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/fatih/gomodifytags@latest
-go install github.com/pressly/goose/v3/cmd/goose@latest
-go install github.com/air-verse/air@latest
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
