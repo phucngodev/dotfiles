36eb3cba09809db84a365086d988d46868cb9572
Phuc <phuc@phucngo.dev>
2024-09-21 11:34:30 +0700

    change breakpoint color

--- init.lua
+++ init.lua
@@ -341,8 +341,7 @@ dap.listeners.before.event_exited.dapui_config = function()
 end
 
 
-vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939' })
-vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef' })
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#f70707' })
 vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379' })
 vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
